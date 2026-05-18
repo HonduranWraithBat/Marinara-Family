66c6f6c28be26f78e5a08713cb6667491f0e2da0
HonduranWraithBat <adriannayg3506@gmail.com>
2026-05-18 14:22:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 const hamburger = document.getElementByld("hamburger");
 const navMenu = document.getElementByld("nav-menu");
 
+// Open and close menu
 hamburger.addEventListener("click", () => {
+  
   navMenu.classList.toggle("active");
-  hamburger.textContent = navMenu.classList.contains("active") ? "X" : "☰";
+
+  //Change icon when menu opens
+ if ( navMenu.classList.toggle("active")) {
+  hamburger.textContent = "X";
+ } else {
+   hamburger.textContent = "☰";
 });
